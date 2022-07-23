cb655833515d4bbf5d4f9d455529a0761bfe5ca0
MangalDutta <98678735+MangalDutta@users.noreply.github.com>
2022-07-24 02:31:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+﻿const config = {
+  subscriptionId: "cb3a7a37-4fd6-4038-9622-1fd3eaa97bcf",
+  resourceGroupName: "SQLOnWindowBox",
+  accountName: "cosmosusecasetestingaccount",
+  databaseName: "AutoScaleTest",
+  containerName: "testcontainer",
+  storedProcedureName: "CreateSPUsingCosmosDBMgmtClient2",
+  partitionKey: { kind: "Hash", paths: ["/<name>"] }
+};
+
+module.exports = config;
