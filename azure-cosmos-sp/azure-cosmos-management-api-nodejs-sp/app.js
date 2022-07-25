dca93c34d91d124b8d94df790d71e197867daa18
MangalDutta <98678735+MangalDutta@users.noreply.github.com>
2022-07-25 11:15:05 +0530

    Add files via upload

--- azure-cosmos-sp/azure-cosmos-management-api-nodejs-sp/app.js
+++ azure-cosmos-sp/azure-cosmos-management-api-nodejs-sp/app.js
@@ -1,1 +1,27 @@
+const { DefaultAzureCredential } = require("@azure/identity");
+const { CosmosDBManagementClient } = require("@azure/arm-cosmosdb");
+const config = require("./config");
+const { subscriptionId, creds,resourceGroupName,accountName,databaseId,containerId } = config;
+
+
+// Use 'DefaultAzureCredential' or any other credential of your choice based on https://aka.ms/azsdk/js/identity/examples
+
+const client = new CosmosDBManagementClient(creds, subscriptionId);
+client.databaseAccounts.get(resourceGroupName, accountName).then((result) => {
+  console.log("The result is:");
+  console.log(result);
+}).catch((err) => {
+  console.log("An error occurred:");
+  console.error(err);
+});
+
+
+async function run() {
+  const { database } = await client.databases.createIfNotExists({ id: databaseId });
+
+  logStep(`Create container with id : ${containerId}`);
+  await database.containers.createIfNotExists({ id: containerId });
+
+
+
 
